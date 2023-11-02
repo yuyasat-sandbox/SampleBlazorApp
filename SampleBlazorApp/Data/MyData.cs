700bd104d563e81794bd83af31776c7206cf493e
yuyasat <takiyour@gmail.com>
2023-11-03 08:25:13 +0900

    form bind MyData

---
+++ SampleBlazorApp/Data/MyData.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleBlazorApp.Data
+{
+    public class MyData
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Mail { get; set; }
+
+        public MyData(string Name, string Password, string Mail)
+        {
+            this.Name = Name ?? "my name.";
+            this.Password = Password ?? "";
+            this.Mail = Mail ?? "";
+        }
+
+        public override string ToString()
+        {
+            return "[" + Name + "(" + Password + ")" + Mail + "]";
+        }
+    }
+}
+
