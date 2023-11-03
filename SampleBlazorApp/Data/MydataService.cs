728db3dd2f2216c92b6721a7d68c9168fc3bc85f
yuyasat <takiyour@gmail.com>
2023-11-03 16:24:00 +0900

    introduce MydataService

---
+++ SampleBlazorApp/Data/MydataService.cs
@@ -0,0 +1,18 @@
+﻿namespace SampleBlazorApp.Data;
+
+public class MydataService
+{
+    private List<MyData> datos = new()
+    {
+        new MyData("Taro","hoge", "taro@yamada"),
+        new MyData("Hanako", "foo", "hanako@flower"),
+        new MyData("Schiko", "bar", "sachiko@happy")
+    };
+
+    public Task<MyData> GetMydataAsync(int n)
+    {
+        int num = n < 0 ? 0 : n > datos.Count ? datos.Count - 1 : n;
+        return Task.FromResult(datos[num]);
+    }
+}
+
